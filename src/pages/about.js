027f2e3d52fb89b8cc0e827de9244daea826de06
Lara Isak <luckasti.larry@gmail.com>
2021-03-06 23:15:30 +0100

    add link to the contact page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
   return (
     <div>
       <h1>About</h1>
       <p>Hi everyone! Nice to see you on my page.</p>
-      <p>I'm currently participanting in the TechLabs Bootcamp, Web Dev track</p>
-      <p>I'm learning about Gatsby at the moment and couldn't be more excited to build some cool shit with it</p>
+      <p>I'm currently participanting in the TechLabs Bootcamp, Web Dev track.</p>
+      <p>I'm learning about Gatsby at the moment and couldn't be more excited to build some cool shit with it.</p>
+      <p>If you would like to send me an email or check my Github greens, you can do that <Link to="/contact">here</Link></p>
     </div>
   )
 }
