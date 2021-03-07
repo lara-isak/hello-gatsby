589a981508075abac8523d6525e040161739ad72
Lara Isak <luckasti.larry@gmail.com>
2021-03-07 15:50:01 +0100

    add header and footer to all pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Footer from './components/footer'
+import Header from './components/header'
 
 const AboutPage = () => {
   return (
     <div>
+      <Header />
       <h1>About</h1>
       <p>Hi everyone! Nice to see you on my page.</p>
       <p>I'm currently participanting in the TechLabs Bootcamp, Web Dev track.</p>
       <p>I'm learning about Gatsby at the moment and couldn't be more excited to build some cool shit with it.</p>
       <p>If you would like to send me an email or check my Github greens, you can do that <Link to="/contact">here</Link></p>
+      <Footer />
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Footer from './components/footer'
+import Header from './components/header'
 
 const ContactPage = () => {
   return (
     <div>
+      <Header />
       <h1>Contact</h1>
       <p>Email: <a href="mailto:luckasti.larry@gmail.com">luckasti.larry@gmail.com</a></p>
       <p>Github: <a href="https://github.com/shewhoridesapony" target="_blank">@shewhoridesapony</a></p>
+      <Footer />
     </div>
   )
 }
