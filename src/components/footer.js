a519c86fb8de4a24ba0d59b91898619ab1f68f23
Lara Isak <luckasti.larry@gmail.com>
2021-05-02 23:33:47 +0200

    🎨 refactor: add contact icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import * as layoutStyles from './layout.module.scss'
+import * as footerStyles from './footer.module.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub, faLinkedin, faTwitter} from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons"
+
 
 const Footer = () => {
   return (
     <footer>
-      <p>Created by Lara with <span className={layoutStyles.heart}>&#10084;</span></p>
+      <div className={footerStyles.footer}>
+        <FontAwesomeIcon icon={faGithub} size="2x" className={footerStyles.icon}/>
+        <FontAwesomeIcon icon={faTwitter} size="2x" className={footerStyles.icon}/>
+        <FontAwesomeIcon icon={faLinkedin} size="2x" className={footerStyles.icon}/>
+        <FontAwesomeIcon icon={faEnvelope} size="2x" className={footerStyles.icon}/>
+      </div>
     </footer>
   )
 }
