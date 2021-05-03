5e695a61688564eda13807537dfb431f646af860
Lara Isak <luckasti.larry@gmail.com>
2021-05-03 16:32:24 +0200

    🚨 fix: add missing role & aria-label to emojis

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
 const IndexPage = () => {
   return(
       <Layout>
-      <h2>Hi there! 👋</h2> 
-      <h2>I'm Lara, a (prefix to be defined) web developer in making, living in Berlin.</h2>
-      <p>Thank you for taking your time to join me on my journey.</p>
-      <p>It will be a bumpy ride so better fasten your seatbelts! 🚀</p>
+        <h2>Hi there! <span role="img" aria-label="wave emoji">👋</span></h2> 
+        <h2>I'm Lara, a (prefix to be defined) web developer in making, living in Berlin.</h2>
+        <p>Thank you for taking your time to join me on my journey.</p>
+        <p>It will be a bumpy ride so better fasten your seatbelts! <span role="img" aria-label="rocket emoji">🚀</span></p>
       </Layout>
   )
 }
