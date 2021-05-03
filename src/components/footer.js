0afb69f8ff4fa85dc78aa8fb2cfbcc0d48c5de07
Lara Isak <luckasti.larry@gmail.com>
2021-05-03 16:25:22 +0200

    ✨ feat: add links to footer icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,10 +9,13 @@ const Footer = () => {
   return (
     <footer>
       <div className={footerStyles.footer}>
-        <FontAwesomeIcon icon={faGithub} size="2x" className={footerStyles.icon}/>
-        <FontAwesomeIcon icon={faTwitter} size="2x" className={footerStyles.icon}/>
-        <FontAwesomeIcon icon={faLinkedin} size="2x" className={footerStyles.icon}/>
-        <FontAwesomeIcon icon={faEnvelope} size="2x" className={footerStyles.icon}/>
+        <a href="https://github.com/lara-isak" target="_blank" rel="noreferrer" aria-label="Github Icon"><FontAwesomeIcon icon={faGithub} size="2x" className={footerStyles.icon}/></a>
+
+        <a href="https://twitter.com/lara_isak" target="_blank" rel="noreferrer" aria-label="Twitter Icon"><FontAwesomeIcon icon={faTwitter} size="2x" className={footerStyles.icon}/></a>
+
+        <a href="https://www.linkedin.com/in/lara-isak/" target="_blank" rel="noreferrer" aria-label="Linkedin Icon"><FontAwesomeIcon icon={faLinkedin} size="2x" className={footerStyles.icon}/></a>
+
+        <a href="mailto:luckasti.larry@gmail.com" target="_blank" rel="noreferrer" aria-label="Email Icon"><FontAwesomeIcon icon={faEnvelope} size="2x" className={footerStyles.icon}/></a>
       </div>
     </footer>
   )
