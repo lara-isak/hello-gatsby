f402e9bfcecfc6e540511bfee6653c2ed79833ce
Lara Isak <luckasti.larry@gmail.com>
2021-05-03 16:29:04 +0200

    🔥 refactor: remove useStaticQuery hook to retrieve data

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,17 @@
 import React from 'react'
-import { Link, graphql, useStaticQuery} from 'gatsby'
+import { Link } from 'gatsby'
 import * as headerStyles from './header.module.scss'
 
 const Header = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          author
-        }
-      }
-    }
-  `)
-
   return (
     <header className={headerStyles.header}>
-      <h1><Link className={headerStyles.title} to="/">{data.site.siteMetadata.author}</Link></h1>
+      <h1><Link className={headerStyles.title} to="/">Lara Isak</Link></h1>
       <nav>
         <ul className={headerStyles.navList}>
           <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link></li>
-          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About Me</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/projects">Projects</Link></li>
           <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
-          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact Me</Link></li>
         </ul>
       </nav>   
     </header>
