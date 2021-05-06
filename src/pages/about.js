62141fb6cbf10ec36367ac86aa77b1039a530d4d
Lara Isak <luckasti.larry@gmail.com>
2021-05-06 21:48:06 +0200

    📝 update text

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import { Link } from 'gatsby'
+// import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
 const AboutPage = () => {
   return (
     <div>
       <Layout>
-        <h2>About Me</h2>
+        <h2>About</h2>
+        <p>My name is Lara and I'm an under construction developer.</p>
+        
+        <p>I come from beautiful, sunny Split, Croatia, but currently living in Berlin. I've been working as a IT Support Engineer / Project Manager for the past 10ish years and decided it was the right time to change my career path and embark on the web dev journey.<span role="img" aria-label="programmer emoji">👩‍💻</span></p>
+        
+        <p>I started following The Odin Project Foundations path and after that path is finished I'll continue with the TOP JavaScript path and Full Stack Open path.</p>
+        
         <p>I'm learning Gatsby + Contentful at the moment to build this portfolio + blog site and couldn't be more excited about the process.</p>
-        <p>If you would like to send me an email or check my Github greens, 
-        you can do that <Link to="/contact">here</Link></p>
+
       </Layout>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => {
   return(
       <Layout>
         <h2>Hi there! <span role="img" aria-label="wave emoji">👋</span></h2> 
-        <h2>I'm Lara, a (prefix to be defined) web developer in making, living in Berlin.</h2>
+        <h2>I'm Lara, an under construction developer, currently living in Berlin.</h2>
         <p>Thank you for taking your time to join me on my journey.</p>
         <p>It will be a bumpy ride so better fasten your seatbelts! <span role="img" aria-label="rocket emoji">🚀</span></p>
       </Layout>
